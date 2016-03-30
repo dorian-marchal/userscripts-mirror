c27f0f8cb8ccf55b9f0ed3b550e2a2a7e3f0226e
dorian-marchal <marchal.dorian@gmail.com>
2016-03-30 12:25:00 +0200

    Add a toggle button

--- env-switcher.user.js
+++ env-switcher.user.js
@@ -3,7 +3,7 @@
 // @namespace   dorian.marchal
 // @include     http://*.jeuxvideo.com/*
 // @require     http://code.jquery.com/jquery-2.2.0.min.js
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -19,7 +19,7 @@ var createButton = function(domain, name) {
   var currentDomain = location.href.match(/^https?:\/\/([a-z]+)\./)[1];
 
   return $('<a>', {
-    class: '' + (currentDomain === domain ? 'primary' : 'default'),
+    class: 'btn-env-switch ' + (currentDomain === domain ? 'primary' : 'default'),
     text: name,
     href: location.href.replace(/^(https?:\/\/)[a-z]+(\..*)/, '$1' + domain + '$2'),
     css: {
@@ -44,10 +44,38 @@ var $container = $('<div>', {
 });
 
 $('body').append($container);
+var $toggleBtn = $('<a>', {
+  text: '>',
+  css: {
+    fontSize: '12px',
+    display: 'inline-block',
+    backgroundColor: '#555',
+    color: '#EEE',
+    padding: '1px 3px',
+    marginRight: '3px',
+    cursor: 'pointer',
+  },
+  click: function() {
+    var $envBtns = $('.btn-env-switch');
+    $envBtns.toggle();
+    var isVisible = $envBtns.first().is(':visible');
+    if (isVisible) {
+      $(this).text('>');
+    } else {
+      $(this).text('<');
+    }
+    localStorage.setItem('env-switcher', isVisible ? 'on' : 'off');
+  },
+});
+$container.append($toggleBtn);
 $container.append(createButton('dorianm', 'Dorian'));
 $container.append(createButton('demo', 'Démo'));
 $container.append(createButton('seo', 'SEO'));
 $container.append(createButton('preprod', 'Préprod'));
 $container.append(createButton('www', 'Prod'));
+
+if (localStorage.getItem('env-switcher') === 'off') {
+  $toggleBtn.click();
+}
 
 })($j);
