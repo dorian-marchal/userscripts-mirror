5e6682a0126a48fe69fc4b3a58b7df9fd4142b1c
Dorian Marchal <marchal.dorian@gmail.com>
2018-04-26 09:31:17 +0200

    Add prettier and promisify GM_xhr

--- tool/userscript/jira-links.user.js
+++ tool/userscript/jira-links.user.js
@@ -8,62 +8,68 @@
 // @connect      github.com
 // ==/UserScript==
 
-'use strict';
-
 const parser = new window.DOMParser();
 const alreadyReplacedClass = '__REPLACED__';
+
+// FIXME URL suffixes, lines, etc.
+
+//  FIXME promises
 const pageNames = {};
 
-function getPageName(pageUrl, extractNameFromDocument, callback) {
-    if (pageNames[pageUrl]) {
-        setImmediate(() => callback(pageNames[pageUrl]));
-    }
-
+const crossOriginRequest = (url) =>
+  new Promise((resolve, reject) => {
     GM_xmlhttpRequest({
-        url: pageUrl,
-        method: 'GET',
-        onload: (response) => {
-            const doc = parser.parseFromString(response.responseText, 'text/html');
-            pageNames[pageUrl] = extractNameFromDocument(doc);
-            callback(pageNames[pageUrl]);
-        }
+      url,
+      method: 'GET',
+      onload: resolve,
+      onerror: reject
     });
+  });
+
+// FIXME async
+function getPageName(pageUrl, extractNameFromDocument, callback) {
+  if (pageNames[pageUrl]) {
+    setImmediate(() => callback(pageNames[pageUrl]));
+  }
+
+  crossOriginRequest(pageUrl).then((response) => {
+    const doc = parser.parseFromString(response.responseText, 'text/html');
+    pageNames[pageUrl] = extractNameFromDocument(doc);
+    callback(pageNames[pageUrl]);
+  });
 }
 
 const nameExtractorCreatorByPattern = {
-    '^https://jira.webedia.fr/browse/(.*)$': (jiraId) => (doc) => {
-        const titleElement = doc.querySelector('#summary-val');
-        return titleElement ? `${jiraId} ${titleElement.textContent}` : null;
-    },
-    '^https://github.com/.*?/(.*?)/pull/(\\d+)': (projectName, prId) => (doc) => {
-        const titleElement = doc.querySelector('h1.gh-header-title span');
-        return titleElement ? `PR ${titleElement.textContent.trim()} (${projectName}#${prId})` : null;
-    },
+  '^https://jira.webedia.fr/browse/(.*)$': (jiraId) => (doc) => {
+    const titleElement = doc.querySelector('#summary-val');
+    return titleElement ? `${jiraId} ${titleElement.textContent}` : null;
+  },
+  '^https://github.com/.*?/(.*?)/pull/(\\d+)': (projectName, prId) => (doc) => {
+    const titleElement = doc.querySelector('h1.gh-header-title span');
+    return titleElement ? `PR ${titleElement.textContent.trim()} (${projectName}#${prId})` : null;
+  }
 };
 
 function replaceLinksText() {
-    window.document.querySelectorAll(`.c-message__body a:not(.${alreadyReplacedClass})`).forEach((link) => {
-        const linkText = link.textContent.trim();
-        Object.keys(nameExtractorCreatorByPattern).forEach((pattern) => {
-            const matches = linkText.match(new RegExp(pattern));
+  window.document.querySelectorAll(`.c-message__body a:not(.${alreadyReplacedClass})`).forEach((link) => {
+    const linkText = link.textContent.trim();
+    Object.keys(nameExtractorCreatorByPattern).forEach((pattern) => {
+      const matches = linkText.match(new RegExp(pattern));
 
-            if (!matches) {
-                link.classList.add(alreadyReplacedClass);
-                return;
-            }
+      if (!matches) {
+        link.classList.add(alreadyReplacedClass);
+        return;
+      }
 
-            getPageName(linkText, nameExtractorCreatorByPattern[pattern](...matches.slice(1)), (pageName) => {
-                if (pageName) {
-                    link.innerHTML = pageName;
-                }
-                link.classList.add(alreadyReplacedClass);
-            });
-        });
+      getPageName(linkText, nameExtractorCreatorByPattern[pattern](...matches.slice(1)), (pageName) => {
+        if (pageName) {
+          link.innerHTML = pageName;
+        }
+        link.classList.add(alreadyReplacedClass);
+      });
     });
+  });
 }
 
 var observer = new MutationObserver(replaceLinksText);
-observer.observe(
-    document.querySelector('.client_main_container'),
-    { subtree: true, childList: true }
-);
+observer.observe(document.querySelector('.client_main_container'), { subtree: true, childList: true });
