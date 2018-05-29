77a0071e4b4628066d8ce4695877346ebe7bbcfb
Dorian Marchal <marchal.dorian@gmail.com>
2018-05-29 11:11:11 +0200

    Add recent workspaces

--- tool/userscript/workspace-switcher.user.js
+++ tool/userscript/workspace-switcher.user.js
@@ -1,13 +1,19 @@
 // ==UserScript==
 // @name         Respawn Workspace Switcher
-// @version      0.1
+// @version      0.2
 // @description  Adds workspace switcher in Respawn debug bar
+// @updateURL    https://github.com/dorian-marchal/phoenix/raw/userscript-jira-links/tool/userscript/workspace-switcher.user.js
+// @downloadURL  https://github.com/dorian-marchal/phoenix/raw/userscript-jira-links/tool/userscript/workspace-switcher.user.js
 // @match        http://*.jeuxvideo.com/*
 // @match        https://*.jeuxvideo.com/*
 // @grant        none
 // ==/UserScript==
 
-const workspaces = [
+const removeDuplicates = (array) => array.filter((v, i, a) => a.indexOf(v) === i);
+
+const recentWorkspacesCookieName = '__RECENT_WORKSPACES__';
+
+const defaultWorkspaces = [
   'www',
   'dorianm1-www.dev',
   'demo1-www.dev',
@@ -17,6 +23,15 @@ const workspaces = [
 ];
 
 const currentWorkspace = location.hostname.match(/^(.*?)\.jeuxvideo.com/)[1];
+
+// Get and update recent workspaces.
+const workspaceCookieMatches = document.cookie.match(new RegExp(`${recentWorkspacesCookieName}=(.*?)(?:$|;)`));
+let recentWorkspaces = workspaceCookieMatches ? JSON.parse(atob(workspaceCookieMatches[1])) : [];
+recentWorkspaces.unshift(currentWorkspace);
+recentWorkspaces = removeDuplicates(recentWorkspaces);
+document.cookie = `${recentWorkspacesCookieName}=${btoa(JSON.stringify(recentWorkspaces))};domain=.jeuxvideo.com;path=/`;
+
+const workspaces = removeDuplicates([...defaultWorkspaces, ...recentWorkspaces]);
 
 const debugBar = document.querySelector('#content-debug');
 
