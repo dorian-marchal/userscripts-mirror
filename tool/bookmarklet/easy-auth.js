31cb1b2ef7dd70e4b33bbaacafea83ac1bfd08e9
Dorian Marchal <marchal.dorian@gmail.com>
2018-05-31 13:00:41 +0200

    Add expire date

---
+++ tool/bookmarklet/easy-auth.js
@@ -0,0 +1,1 @@
+javascript:const CONIUNCTIO='__VALUE__';document.cookie=`coniunctio=${CONIUNCTIO};domain=${location.hostname};path=/;Expires=Wed, 23 Oct 2030 07:28:00 GMT;`;location.reload();
--- tool/userscript/workspace-switcher.user.js
+++ tool/userscript/workspace-switcher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Respawn Workspace Switcher
-// @version      0.2
+// @version      0.11
 // @description  Adds workspace switcher in Respawn debug bar
 // @updateURL    https://github.com/dorian-marchal/phoenix/raw/userscript-jira-links/tool/userscript/workspace-switcher.user.js
 // @downloadURL  https://github.com/dorian-marchal/phoenix/raw/userscript-jira-links/tool/userscript/workspace-switcher.user.js
@@ -11,32 +11,49 @@
 
 const removeDuplicates = (array) => array.filter((v, i, a) => a.indexOf(v) === i);
 
-const recentWorkspacesCookieName = '__WORKSPACES_v1__';
+const debugBar = document.querySelector('#content-debug');
+
+if (debugBar === null) {
+  return;
+}
+
+const MAX_WORKSPACE_COUNT = 30;
+const RECENT_WORKSPACES_COOKIE_NAME = '__WORKSPACES_v1__';
+const WORKSPACE_SEPARATOR = '---------------';
 
 const defaultWorkspaces = [
   'www',
+  'preprod-www',
   'dorianm1-www.dev',
-  'demo1-www.dev',
   'adrienw1-www.dev',
   'antoinez1-www.dev',
-  'bricel1-www.dev'
+  'bricel1-www.dev',
+  'demo1-www.dev',
+  'demo2-www.dev',
+  WORKSPACE_SEPARATOR
 ];
 
 const currentWorkspace = location.hostname.match(/^(.*?)\.jeuxvideo.com/)[1];
 
-// Get and update recent workspaces.
-const workspaceCookieMatches = document.cookie.match(new RegExp(`${recentWorkspacesCookieName}=(.*?)(?:$|;)`));
-let recentWorkspaces = workspaceCookieMatches ? JSON.parse(atob(workspaceCookieMatches[1])) : [];
-recentWorkspaces.unshift(currentWorkspace);
-recentWorkspaces = removeDuplicates(recentWorkspaces);
-document.cookie = `${recentWorkspacesCookieName}=${btoa(JSON.stringify(recentWorkspaces))};domain=.jeuxvideo.com;path=/`;
+// Get recent workspaces.
+const workspaceCookieMatches = document.cookie.match(new RegExp(`${RECENT_WORKSPACES_COOKIE_NAME}=(.*?)(?:$|;)`));
+let recentWorkspaces = workspaceCookieMatches
+  ? JSON.parse(atob(workspaceCookieMatches[1])).slice(0, MAX_WORKSPACE_COUNT)
+  : [];
+
+// Update recent workspaces
+if (currentWorkspace.endsWith('.dev')) {
+  recentWorkspaces.unshift(currentWorkspace);
+  recentWorkspaces = removeDuplicates(recentWorkspaces);
+  document.cookie = `${RECENT_WORKSPACES_COOKIE_NAME}=${btoa(
+    JSON.stringify(recentWorkspaces)
+  )};domain=.jeuxvideo.com;path=/;Expires=Wed, 23 Oct 2030 07:28:00 GMT;`;
+}
 
 const workspaces = removeDuplicates([...defaultWorkspaces, ...recentWorkspaces]);
 
-const debugBar = document.querySelector('#content-debug');
-
 const workspaceList = document.createElement('select');
-workspaceList.style = 'height: 100%;';
+workspaceList.style = 'height: 100%;max-width:280px';
 
 const defaultOption = document.createElement('option');
 defaultOption.label = 'Workspace...';
@@ -45,8 +62,12 @@ workspaceList.appendChild(defaultOption);
 workspaces.forEach((workspace) => {
   const option = document.createElement('option');
   option.label = workspace;
-  option.value = workspace;
-  option.selected = currentWorkspace === workspace;
+  if (workspace === WORKSPACE_SEPARATOR) {
+    option.disabled = true;
+  } else {
+    option.value = workspace;
+    option.selected = currentWorkspace === workspace;
+  }
   workspaceList.appendChild(option);
 });
 
