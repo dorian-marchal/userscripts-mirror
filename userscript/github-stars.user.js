d2ebf926e535b014a2126718d2454b441918cdcb
Dorian Marchal <marchal.dorian@gmail.com>
2018-11-21 17:58:06 +0100

    Improve userscript/github-stars.user.js
    - Exclude anchors.
    - Exclude non-github URLs.
    - Add caching.
    - Remove console.log.
    - Format star counts.

--- userscript/github-stars.user.js
+++ userscript/github-stars.user.js
@@ -1,27 +1,46 @@
 // ==UserScript==
 // @name         github-stars.user
-// @version      0.1
+// @version      0.2
 // @match        https://github.com/*
 // @grant        none
 // ==/UserScript==
 
+const pageHtmlPromises = {};
+
 (function() {
   const showStars = () =>
-    document.querySelectorAll('.markdown-body a').forEach((link) => {
-      if (link.classList.contains('anchor')) return;
-      fetch(link)
-        .then((res) => res.text())
-        .then((html) => {
+    document
+      .querySelectorAll('.markdown-body a:not([href^="#"])')
+      .forEach((link) => {
+        if (link.classList.contains('anchor')) {
+          return;
+        }
+        if (new URL(link.href).hostname !== 'github.com') {
+          return;
+        }
+        if (!pageHtmlPromises[link.href]) {
+          pageHtmlPromises[link.href] = fetch(link.href).then((res) =>
+            res.text()
+          );
+        }
+        pageHtmlPromises[link.href].then((html) => {
           const doc = new DOMParser().parseFromString(html, 'text/html');
           const starCount = parseInt(
-            doc.querySelector('[aria-label$=" users starred this repository"]').getAttribute('aria-label'),
+            doc
+              .querySelector('[aria-label$=" users starred this repository"]')
+              .getAttribute('aria-label'),
             10
           );
-          console.log(starCount);
-          const color = starCount > 500 ? 'green' : starCount > 100 ? 'orange' : 'red';
-          link.insertAdjacentHTML('afterend', ` <span style="color:${color};">(${starCount} ★)</span>`);
+          const color =
+            starCount > 500 ? 'green' : starCount > 100 ? 'orange' : 'red';
+          link.insertAdjacentHTML(
+            'afterend',
+            ` <span style="color:${color};">(${Intl.NumberFormat(
+              'fr-FR'
+            ).format(starCount)}&nbsp;★)</span>`
+          );
         });
-    });
+      });
 
   const button = document.createElement('button');
   button.innerText = 'Show ★';
