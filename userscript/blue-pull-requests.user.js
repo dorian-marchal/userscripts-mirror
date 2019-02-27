80bc5c50f6a015881f354a5d6c61aa03b47440a1
Dorian Marchal <marchal.dorian@gmail.com>
2019-02-27 15:34:07 +0100

    Add PR count

--- userscript/blue-pull-requests.user.js
+++ userscript/blue-pull-requests.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         blue-pull-requests.user
-// @version      4
+// @version      5
 // @match        https://github.com/search?o=desc&s=updated&p=1&q=-label%3A%E2%98%A0+-label%3Aold+org%3AWebediaGaming+is%3Apr+is%3Aopen+author%3Adorian-marchal++author%3AadrienWeiss+author%3Abibislayer+author%3Aantca+author%3Aericroge-webedia+author%3ARomainCorralJVC&type=Issues
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -20,13 +20,13 @@ document.querySelectorAll('.issue-list-item span.ml-3').forEach((item) => {
 const respawnLink = document.createElement('a');
 respawnLink.href =
   `https://github.com/WebediaGaming/jvc-respawn/pulls?utf8=%E2%9C%93&q=is%3Apr+is%3Aopen+in%3Acomments+` + ids.join('+');
-respawnLink.textContent = '- Liste des PR sur Respawn';
+respawnLink.textContent = `- Liste des PR sur Respawn (${ids.length})`;
 respawnLink.className = 'project-link';
 
 const phoenixLink = document.createElement('a');
 phoenixLink.href =
   `https://github.com/WebediaGaming/phoenix/pulls?utf8=%E2%9C%93&q=is%3Apr+is%3Aopen+in%3Acomments+` + ids.join('+');
-phoenixLink.textContent = '- Liste des PR sur Phoenix';
+phoenixLink.textContent = `- Liste des PR sur Phoenix (${ids.length})`;
 phoenixLink.className = 'project-link';
 
 const listWrapper = document.querySelector('#issue_search_results');
